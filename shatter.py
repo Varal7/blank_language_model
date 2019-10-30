d410d9668d69c06321cbeda7a8d6dcc1ec12bd09
Tianxiao Shen <tianxiao@30-9-240.wireless.csail.mit.edu>
2019-10-30 16:23:36 -0400

    shatter sentences to blank and fill

--- shatter.py
+++ shatter.py
@@ -1,14 +1,37 @@
 import sys
 import random
-from utils import load_sent
+
+def load_sent(path):
+    sents = []
+    with open(path) as f:
+        for line in f:
+            sents.append(line.split())
+    return sents
 
 path = sys.argv[1]
-sents = load_sent(path)
-with open(path + '.shatter', 'w') as f:
-    for s in sents:
-        k = random.randint(1, len(s))
-        order = list(range(len(s)))
-        random.shuffle(order)
-        keep = order[:k]
-        s_ = [w for i, w in enumerate(s) if i in keep]
-        f.write(' '.join(s_) + '\n')
+sents = load_sent(path + '.txt')
+with open(path + '.blank', 'w') as fb:
+    with open(path + '.fill', 'w') as ff:
+        for s in sents:
+            k = random.randint(0, len(s))
+            order = list(range(len(s)))
+            random.shuffle(order)
+            keep = order[:k]
+
+            blank, fill = [], []
+            for i, w in enumerate(s):
+                if i in keep:
+                    blank.append(w)
+                    if not (len(fill) > 0 and fill[-1] == '<sep>'):
+                        fill.append('<sep>')
+                else:
+                    if not (len(blank) > 0 and blank[-1] == '<blank>'):
+                        blank.append('<blank>')
+                    fill.append(w)
+            if len(fill) > 0 and fill[0] == '<sep>':
+                fill = fill[1:]
+            if len(fill) > 0 and fill[-1] == '<sep>':
+                fill = fill[:-1]
+
+            fb.write(' '.join(blank) + '\n')
+            ff.write(' '.join(fill) + '\n')
