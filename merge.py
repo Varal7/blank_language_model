5f906bcb03e23e559f83f8034cfcefe40b9ac8c9
Tianxiao Shen <tianxiao@dhcp-18-21-214-224.dyn.mit.edu>
2019-10-31 21:39:07 -0400

    shatter and merge

---
+++ merge.py
@@ -0,0 +1,22 @@
+import sys
+import re
+
+def load(path):
+    with open(path) as f:
+        lines = f.readlines()
+    return [l.strip() for l in lines]
+
+blank = load(sys.argv[1])
+fill = load(sys.argv[2])
+with open(sys.argv[2] + '.merge', 'w') as fout:
+    for b, f in zip(blank, fill):
+        b_ = re.split('<blank>', b)
+        f_ = re.split(' <sep> ', f) if f != '' else []
+        if len(b_) != len(f_) + 1:
+            print('error:\n%s\n%s\n' % (b, f))
+        s = b_[0]
+        for i in range(len(b_)-1):
+            if i < len(f_):
+                s += f_[i]
+            s += b_[i+1]
+        fout.write(s + '\n')
--- shatter.py
+++ shatter.py
@@ -8,30 +8,53 @@ def load_sent(path):
             sents.append(line.split())
     return sents
 
-path = sys.argv[1]
-sents = load_sent(path + '.txt')
-with open(path + '.blank', 'w') as fb:
-    with open(path + '.fill', 'w') as ff:
+def write(sents, path):
+    with open(path, 'w') as f:
         for s in sents:
-            k = random.randint(0, len(s))
-            order = list(range(len(s)))
-            random.shuffle(order)
-            keep = order[:k]
+            f.write(' '.join(s) + '\n')
 
-            blank, fill = [], []
-            for i, w in enumerate(s):
-                if i in keep:
-                    blank.append(w)
-                    if not (len(fill) > 0 and fill[-1] == '<sep>'):
-                        fill.append('<sep>')
-                else:
-                    if not (len(blank) > 0 and blank[-1] == '<blank>'):
-                        blank.append('<blank>')
-                    fill.append(w)
-            if len(fill) > 0 and fill[0] == '<sep>':
-                fill = fill[1:]
-            if len(fill) > 0 and fill[-1] == '<sep>':
-                fill = fill[:-1]
+def shatter(sent):
+    k = random.randint(0, len(sent))
+    order = list(range(len(sent)))
+    random.shuffle(order)
+    keep = order[:k]
 
-            fb.write(' '.join(blank) + '\n')
-            ff.write(' '.join(fill) + '\n')
+    drop, blank, fill = [], [], []
+    for i, w in enumerate(sent):
+        if i in keep:
+            drop.append(w)
+            blank.append(w)
+            if not (len(fill) > 0 and fill[-1] == '<sep>'):
+                fill.append('<sep>')
+        else:
+            if not (len(blank) > 0 and blank[-1] == '<blank>'):
+                blank.append('<blank>')
+            fill.append(w)
+    if len(fill) > 0 and fill[0] == '<sep>':
+        fill = fill[1:]
+    if len(fill) > 0 and fill[-1] == '<sep>':
+        fill = fill[:-1]
+
+    return drop, blank, fill
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    sents = load_sent(path + '.txt')
+    random.seed(1)
+
+    n = 1
+    if len(sys.argv) > 2:
+        n = int(sys.argv[2])
+        path += str(n)
+
+    drop, blank, fill = [], [], []
+    for i in range(n):
+        for sent in sents:
+            d, b, f = shatter(sent)
+            drop.append(d)
+            blank.append(b)
+            fill.append(f)
+
+    #write(drop, path + '.drop')
+    write(blank, path + '.blank')
+    write(fill, path + '.fill')
