07a3ede330589da378bb9dc2aad5966bea73412d
Tianxiao Shen <tianxiao@dhcp-18-21-214-224.dyn.mit.edu>
2019-11-03 22:45:08 -0500

    blank model

--- model.py
+++ model.py
@@ -34,24 +34,24 @@ def get_canvas(seq, keep, __):
     bs, n = seq.size(0), seq.size(1)
     ___ = torch.zeros(bs, 1, dtype=torch.long, device=seq.device).fill_(__)
     canvas = torch.zeros(bs, 0, dtype=torch.long, device=seq.device)
-    blanks, rest, pos, l_, r_ = [], [], [], [], []
+    blanks, rest, pos, lb, rb = [], [], [], [], []
     i = 0
     while i < n:
         if i in keep:
             canvas = torch.cat((canvas, seq[:, i:i+1]), dim=1)
             i += 1
         else:
-            blanks.append(canvas.size(1))
             a = []
             while i < n and i not in keep:
                 rest.append(i)
-                pos.append(canvas.size(1))
+                pos.append(len(blanks))
                 a.append(1)
                 i += 1
-            l_ += [0] + a[1:]
-            r_ += a[:-1] + [0]
+            lb += [0] + a[1:]
+            rb += a[:-1] + [0]
+            blanks.append(canvas.size(1))
             canvas = torch.cat((canvas, ___), dim=1)
-    return canvas, blanks, rest, pos, l_, r_
+    return canvas, blanks, rest, pos, lb, rb
 
 class LM(nn.Module):
     """Language Model"""
@@ -62,7 +62,7 @@ class LM(nn.Module):
         self.args = args
 
         self.log_factorial = [0]
-        for i in range(1, args.max_len):
+        for i in range(1, args.max_len+1):
             self.log_factorial.append(self.log_factorial[-1] + np.log(i))
 
         self.G = Encoder(
@@ -71,16 +71,9 @@ class LM(nn.Module):
             n_layers=args.n_layers, n_head=args.n_head, d_k=args.d_k, d_v=args.d_v,
             dropout=args.dropout)
 
-        self.tgt_word_prj = nn.Linear(2*args.d_model, vocab.size, bias=False)
-        nn.init.xavier_normal_(self.tgt_word_prj.weight)
-
-        if args.share_emb_prj_weight:
-            self.tgt_word_prj.weight = self.G.src_word_emb.weight
-            self.x_logit_scale = (args.d_model ** -0.5)
-        else:
-            self.x_logit_scale = 1.
-
-        self.loc = nn.Linear(2*args.d_model, 1, bias=False)
+        self.word_lb_rb = nn.Linear(args.d_model, vocab.size*4, bias=False)
+        nn.init.xavier_normal_(self.word_lb_rb.weight)
+        self.loc = nn.Linear(args.d_model, 1, bias=False)
 
         opt = optim.Adam(self.parameters(), betas=eval(args.adam_betas),
             eps=args.adam_eps, weight_decay=args.weight_decay)
@@ -92,24 +85,20 @@ class LM(nn.Module):
             self.opt = LRScheduler(opt, args.lr)
 
     def forward(self, canvas, blanks):
-        pos = torch.arange(canvas.size(1)).repeat(canvas.size(0), 1).to(seq.device)
+        pos = torch.arange(canvas.size(1)).repeat(canvas.size(0), 1).to(canvas.device)
         output, *_ = self.G(canvas, pos)
-
-        slot = torch.cat((output[:, :-1, :], output[:, 1:, :]), dim=-1)
-        logits_c = self.tgt_word_prj(slot) * self.x_logit_scale
-        logits_l = self.loc(slot).squeeze(-1)
-        return logits_c, logits_l
+        output = output[:, blanks, :]
+        return self.loc(output).squeeze(-1), self.word_lb_rb(output)
 
     def loss(self, seq):
         n = seq.size(1)
         k = np.random.randint(n)
         keep = sorted(np.random.permutation(n)[:k])
-        canvas, blanks, rest, pos, l_, r_ = get_canvas(seq, keep, self.vocab.blank)
-
-        import pdb; pdb.set_trace()
-        logits_c, logits_l = self(canvas, blanks)
-        lp_c = -seq_cross_entropy(logits_c[:, slot, :], seq[:, rest],
+        canvas, blanks, rest, pos, lb, rb = get_canvas(seq, keep, self.vocab.blank)
+        logits_loc, logits_word_lb_rb = self(canvas, blanks)
+        loss_loc = -F.log_softmax(logits_loc, dim=1)[:, pos].mean()
+        index = seq[:, rest]*4 + torch.LongTensor(lb).to(seq.device)*2 + \
+            torch.LongTensor(rb).to(seq.device)
+        loss_word_lb_rb = seq_cross_entropy(logits_word_lb_rb[:, pos, :], index,
             self.vocab.pad, self.args.label_smoothing)
-        lp_l = F.log_softmax(logits_l, dim=1)[:, slot].mean()
-
-        return -self.log_prb(seq, canvas, rest, slot)*(n+1) - self.log_factorial[n]
+        return (loss_loc + loss_word_lb_rb) * n - self.log_factorial[n]
