b9b56d33a65116ac9997b80987c328bb8b6c1235
Tianxiao Shen <tianxiao@dhcp-18-21-192-134.dyn.mit.edu>
2020-01-15 01:35:37 -0500

    count vocab size

--- auxiliary/stats.py
+++ auxiliary/stats.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from collections import Counter
 
 def load_sent(path):
     sents = []
@@ -12,7 +13,12 @@ dir = sys.argv[1]
 for file in ['train.txt', 'valid.txt', 'test.txt']:
     sents = load_sent(os.path.join(dir, file))
     lens = [len(s) for s in sents]
+    cnt = Counter([w for s in sents for w in s])
+
     print('\n%s' % file)
-    print('num of sents:\t %d' % len(sents))
-    print('max length:\t %d' % max(lens))
-    print('avg length:\t %.2f' % (sum(lens) / len(lens)))
+    print('num of sents:      %d' % len(sents))
+    print('num of tokens:     %d' % sum(lens))
+    print('max length:        %d' % max(lens))
+    print('avg length:        %.2f' % (sum(lens) / len(lens)))
+    print('vocab size (>=1):  %d' % len(cnt))
+    print('vocab size (>=5):  %d' % sum([cnt[w] >= 5 for w in cnt]))
