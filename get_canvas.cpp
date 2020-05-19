61a950b7f20077f24b96a1b914a526994dd9c82d
Victor Quach <varaal@gmail.com>
2020-05-19 17:59:37 -0400

    Merge get_canvas.cpp

--- get_canvas.cpp
+++ get_canvas.cpp
@@ -79,7 +79,43 @@ vector<vector<vector<uint32_t>>> get_canvas(
   return {batch_canvas, batch_blanks, batch_rest, batch_loc, batch_lb, batch_rb};
 }
 
+vector<vector<vector<uint32_t>>> get_known_length_canvas(
+    vector<vector<uint32_t>>& seq,
+    vector<vector<bool>>& keep,
+    vector<uint32_t> n,
+    uint32_t blank_id) {
+  vector<vector<uint32_t>> batch_canvas, batch_blanks, batch_rest, batch_loc, batch_lb;
+  for (uint32_t b = 0; b < seq.size(); b++) {
+    vector<uint32_t> canvas, blanks, rest, loc, lb;
+    for (uint32_t i = 0; i < n[b]; ) {
+      if (keep[b][i]) {
+        canvas.push_back(seq[b][i]);
+        i++;
+      } else {
+        uint32_t cur_len = 0;
+        while (i < n[b] && !keep[b][i]) {
+          rest.push_back(i);
+          loc.push_back(blanks.size());
+          lb.push_back(cur_len);
+          i++;
+          cur_len++;
+        }
+        blanks.push_back(canvas.size());
+        canvas.push_back(blank_id + cur_len);
+      }
+    }
+    batch_canvas.push_back(canvas);
+    batch_blanks.push_back(blanks);
+    batch_rest.push_back(rest);
+    batch_loc.push_back(loc);
+    batch_lb.push_back(lb);
+  }
+  return {batch_canvas, batch_blanks, batch_rest, batch_loc, batch_lb};
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("get_canvas", &get_canvas, "get_canvas");
   m.def("get_insertion_canvas", &get_insertion_canvas, "get_insertion_canvas");
+  m.def("get_known_length_canvas", &get_known_length_canvas, "get_known_length_canvas");
+
 }
--- test_cpp.py
+++ test_cpp.py
@@ -3,11 +3,160 @@ import unittest
 import torch
 from torch.utils.cpp_extension import load
 
-batched_get_canvas = load(name="get_canvas_cpp", sources=["get_canvas.cpp"])
+batched_get_canvas = load(name="canvas", sources=["get_canvas.cpp"])
 
 
-def get_canvas(seq, keep, n):
+def get_insertion_canvas(seq, keep, n):
     return [x[0] for x in  batched_get_canvas.get_insertion_canvas([seq.tolist()], [keep.tolist()], [n])]
+
+def get_known_length_canvas(seq, keep, n, blank_id):
+    return [x[0] for x in batched_get_canvas.get_known_length_canvas([seq.tolist()], [keep.tolist()], [n], blank_id)]
+
+def get_canvas(seq, keep, n, blank_id):
+    return [x[0] for x in  batched_get_canvas.get_canvas([seq.tolist()], [keep.tolist()], [n], blank_id)]
+
+
+class TestMakeBlanks(unittest.TestCase):
+    def test_get_canvas_one(self):
+        seq       = torch.tensor([0, 100, 101, 102, 103, 104, 105, 106, 107, 2, 1])
+        keep_mask = torch.tensor([1,   1,   0,   1,   1,   0,   0,   1,   0, 1, 0]).bool()
+        n = 10
+        canvas, blanks, rest, loc, lb, rb = get_canvas(seq, keep_mask, n, 4)
+
+        exp_canvas = [0, 100, 4, 102, 103, 4, 106, 4, 2]
+        exp_blanks = [2, 5, 7]
+        exp_rest   = [2, 5, 6, 8]
+        exp_loc    = [0, 1, 1, 2]
+        exp_lb     = [0, 0, 1, 0]
+        exp_rb     = [0, 1, 0, 0]
+
+        self.assertEqual(exp_canvas, canvas)
+        self.assertEqual(exp_blanks, blanks)
+        self.assertEqual(exp_rest, rest)
+        self.assertEqual(exp_loc, loc)
+        self.assertEqual(exp_lb, lb)
+        self.assertEqual(exp_rb, rb)
+
+
+    def test_get_canvas_two(self):
+        seq       = torch.tensor([0, 100, 101, 102, 103, 104, 105, 106, 107, 2, 1])
+        keep_mask = torch.tensor([1,   1,   0,   1,   1,   0,   0,   1,   1, 1, 0]).bool()
+        n = 10
+        canvas, blanks, rest, loc, lb, rb = get_canvas(seq, keep_mask, n, 4)
+
+        exp_canvas = [0, 100, 4, 102, 103, 4, 106, 107, 2]
+        exp_blanks = [2, 5]
+        exp_rest   = [2, 5, 6]
+        exp_loc    = [0, 1, 1]
+        exp_lb     = [0, 0, 1]
+        exp_rb     = [0, 1, 0]
+
+        self.assertEqual(exp_canvas, canvas)
+        self.assertEqual(exp_blanks, blanks)
+        self.assertEqual(exp_rest, rest)
+        self.assertEqual(exp_loc, loc)
+        self.assertEqual(exp_lb, lb)
+        self.assertEqual(exp_rb, rb)
+
+    def test_get_canvas_three(self):
+        seq       = torch.tensor([0, 200, 201, 202, 203, 204, 205, 206, 207, 2, 1])
+        keep_mask = torch.tensor([1,   0,   1,   0,   1,   0,   1,   0,   1, 1, 0]).bool()
+        n = 10
+        canvas, blanks, rest, loc, lb, rb = get_canvas(seq, keep_mask, n, 4)
+
+        exp_canvas = [0, 4, 201, 4, 203, 4, 205, 4, 207, 2]
+        exp_blanks = [1, 3, 5, 7]
+        exp_rest   = [1, 3, 5, 7]
+        exp_loc    = [0, 1, 2, 3]
+        exp_lb     = [0, 0, 0, 0]
+        exp_rb     = [0, 0, 0, 0]
+
+        self.assertEqual(exp_canvas, canvas)
+        self.assertEqual(exp_blanks, blanks)
+        self.assertEqual(exp_rest, rest)
+        self.assertEqual(exp_loc, loc)
+        self.assertEqual(exp_lb, lb)
+        self.assertEqual(exp_rb, rb)
+
+
+class TestMakeFixedLengthBlanks(unittest.TestCase):
+    def test_get_canvas_one(self):
+        seq       = torch.tensor([0, 100, 101, 102, 103, 104, 105, 106, 107, 2, 1])
+        keep_mask = torch.tensor([1,   1,   0,   1,   1,   0,   0,   1,   0, 1, 0]).bool()
+        n = 10
+        blank_0 = 3
+        canvas, blanks, rest, loc, lb = get_known_length_canvas(seq, keep_mask, n, blank_0)
+
+        exp_canvas = [0, 100, 4, 102, 103, 5, 106, 4, 2]
+        exp_blanks = [2, 5, 7]
+        exp_rest   = [2, 5, 6, 8]
+        exp_loc    = [0, 1, 1, 2]
+        exp_lb     = [0, 0, 1, 0]
+
+        self.assertEqual(exp_canvas, canvas)
+        self.assertEqual(exp_blanks, blanks)
+        self.assertEqual(exp_rest, rest)
+        self.assertEqual(exp_loc, loc)
+        self.assertEqual(exp_lb, lb)
+
+
+    def test_get_canvas_two(self):
+        seq       = torch.tensor([0, 100, 101, 102, 103, 104, 105, 106, 107, 2, 1])
+        keep_mask = torch.tensor([1,   1,   0,   1,   1,   0,   0,   1,   1, 1, 0]).bool()
+        n = 10
+        blank_0 = 3
+        canvas, blanks, rest, loc, lb = get_known_length_canvas(seq, keep_mask, n, blank_0)
+
+        exp_canvas = [0, 100, 4, 102, 103, 5, 106, 107, 2]
+        exp_blanks = [2, 5]
+        exp_rest   = [2, 5, 6]
+        exp_loc    = [0, 1, 1]
+        exp_lb     = [0, 0, 1]
+
+        self.assertEqual(exp_canvas, canvas)
+        self.assertEqual(exp_blanks, blanks)
+        self.assertEqual(exp_rest, rest)
+        self.assertEqual(exp_loc, loc)
+        self.assertEqual(exp_lb, lb)
+
+    def test_get_canvas_three(self):
+        seq       = torch.tensor([0, 200, 201, 202, 203, 204, 205, 206, 207, 2, 1])
+        keep_mask = torch.tensor([1,   0,   1,   0,   1,   0,   1,   0,   1, 1, 0]).bool()
+        n = 10
+        blank_0 = 3
+        canvas, blanks, rest, loc, lb = get_known_length_canvas(seq, keep_mask, n, blank_0)
+
+        exp_canvas = [0, 4, 201, 4, 203, 4, 205, 4, 207, 2]
+        exp_blanks = [1, 3, 5, 7]
+        exp_rest   = [1, 3, 5, 7]
+        exp_loc    = [0, 1, 2, 3]
+        exp_lb     = [0, 0, 0, 0]
+
+        self.assertEqual(exp_canvas, canvas)
+        self.assertEqual(exp_blanks, blanks)
+        self.assertEqual(exp_rest, rest)
+        self.assertEqual(exp_loc, loc)
+        self.assertEqual(exp_lb, lb)
+
+
+    def test_get_canvas_four(self):
+        seq       = torch.tensor([0, 200, 201, 202, 203, 204, 205, 206, 207, 2, 1])
+        keep_mask = torch.tensor([1,   0,   0,   0,   0,   0,   1,   0,   1, 1, 0]).bool()
+        n = 10
+        blank_0 = 3
+        canvas, blanks, rest, loc, lb = get_known_length_canvas(seq, keep_mask, n, blank_0)
+
+        exp_canvas = [0, 8, 205, 4, 207, 2]
+        exp_blanks = [1, 3]
+        exp_rest   = [1, 2, 3, 4, 5, 7]
+        exp_loc    = [0, 0, 0, 0, 0, 1]
+        exp_lb     = [0, 1, 2, 3, 4, 0]
+
+        self.assertEqual(exp_canvas, canvas)
+        self.assertEqual(exp_blanks, blanks)
+        self.assertEqual(exp_rest, rest)
+        self.assertEqual(exp_loc, loc)
+        self.assertEqual(exp_lb, lb)
 
 
 class TestMakeInsertionBlanks(unittest.TestCase):
@@ -15,7 +164,7 @@ class TestMakeInsertionBlanks(unittest.TestCase):
         seq       = torch.tensor([0, 100, 101, 102, 103, 104, 105, 106, 107, 2, 1])
         keep_mask = torch.tensor([1,   1,   0,   1,   1,   0,   0,   1,   0, 1, 0]).bool()
         n = 8
-        canvas, rest, loc = get_canvas(seq, keep_mask, n)
+        canvas, rest, loc = get_insertion_canvas(seq, keep_mask, n)
 
         exp_canvas = [0, 100, 102, 103, 106, 2]
         exp_rest   = [2, 5, 6, 8]
@@ -30,7 +179,7 @@ class TestMakeInsertionBlanks(unittest.TestCase):
         seq       = torch.tensor([0, 100, 101, 102, 103, 104, 105, 106, 107, 2, 1])
         keep_mask = torch.tensor([1,   1,   0,   1,   1,   0,   0,   1,   1, 1, 0]).bool()
         n = 8
-        canvas, rest, loc = get_canvas(seq, keep_mask, n)
+        canvas, rest, loc = get_insertion_canvas(seq, keep_mask, n)
 
         exp_canvas = [0, 100, 102, 103, 106, 107, 2]
         exp_rest   = [2, 5, 6]
@@ -44,7 +193,7 @@ class TestMakeInsertionBlanks(unittest.TestCase):
         seq       = torch.tensor([0, 200, 201, 202, 203, 204, 205, 206, 207, 2, 1])
         keep_mask = torch.tensor([1,   0,   1,   0,   1,   0,   1,   0,   1, 1, 0]).bool()
         n = 8
-        canvas, rest, loc = get_canvas(seq, keep_mask, n)
+        canvas, rest, loc = get_insertion_canvas(seq, keep_mask, n)
 
         exp_canvas = [0, 201, 203, 205, 207, 2]
         exp_rest   = [1, 3, 5, 7]
@@ -58,7 +207,7 @@ class TestMakeInsertionBlanks(unittest.TestCase):
         seq       = torch.tensor([0, 200, 201, 202, 203, 204, 205, 206, 207, 2, 1])
         keep_mask = torch.tensor([1,   1,   1,   1,   1,   1,   1,   1,   1, 1, 0]).bool()
         n = 8
-        canvas, rest, loc = get_canvas(seq, keep_mask, n)
+        canvas, rest, loc = get_insertion_canvas(seq, keep_mask, n)
 
         exp_canvas = [0, 200, 201, 202, 203, 204, 205, 206, 207, 2]
         exp_rest   = [9]
